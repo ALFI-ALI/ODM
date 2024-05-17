b747941d1bdbe70f952826e2f77466a054e8b5fa
ALFI ALI <alfiali.333@gmail.com>
2024-05-17 16:06:31 -0700

    exe
    exe on ODM

---
+++ odm.js
@@ -0,0 +1,9 @@
+// innerHTML = It includes HTML tags
+// innerText = "spaces included only visible for Javascript" 
+// textContent = NO spaces included, also ignores spaces in Javascript
+document.getElementById('message').innerText = "                   Plain text";
+//document.getElementById('message').innerHTML = "<h1>Demo</h1>";
+//document.getElementById('message').textContent = "          Example test";
+// document.getElementByID  (1 per HTML page)
+// document.getElementByClassName (multple classes )
+// document.getElementByTagName  (multiple same tags like <p>)
---
+++ saiyans.js
@@ -0,0 +1,22 @@
+const saiyans = [
+    {
+        name: "Goku",
+        powerLevel: 9000,
+        transformations: ["Super Saiyan", "Super Saiyan God", "Ultra Instinct"]
+    },
+    {
+        name: "Vegeta",
+        powerLevel: 8500,
+        transformations: ["Super Saiyan", "Super Saiyan Blue", "Super Saiyan Blue Evolved"]
+    },
+    {
+        name: "Gohan",
+        powerLevel: 7500,
+        transformations: ["Super Saiyan", "Super Saiyan 2", "Mystic Gohan"]
+    },
+     {
+        name: "Monch",
+        powerLevel: 150000,
+        transformations: ["Super Saiyan", "Super Saiyan Rage"]
+    }
+];
